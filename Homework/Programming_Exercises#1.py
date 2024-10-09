673803727330d940c02857fd068a0571f1900cb1
Esteban <cornejoes@celts.crespi.org>
2024-10-08 20:49:36 -0700

    Add files via upload

---
+++ Homework/Programming_Exercises#1.py
@@ -0,0 +1,13 @@
+#File: Programming_Exercise#1
+#Student: Esteban Cornejo
+#
+#Date: October 8, 2024
+#Description of Program: It is a part of my Programming Exercises,
+# this one is one that displays information about myself such as
+#my name, address, phone number, etc 
+#GitHub: ejcornejo
+
+print("Esteban Cornejo")
+print("20426 Via Urbino, Porter Ranch, CA, 91326")
+print("+1 (818)966-9917")
+print("Pre-Law")
