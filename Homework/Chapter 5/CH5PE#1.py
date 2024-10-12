a04db1f3e52fe905b3c563fd4ebaab67a24e7f3c
Esteban <cornejoes@celts.crespi.org>
2024-10-12 12:45:57 -0700

    Add files via upload

---
+++ Homework/Chapter 5/CH5PE#1.py
@@ -0,0 +1,8 @@
+def convert():
+    kilometer = float(input("Please enter any distance in kilometers: "))
+    
+    mile = kilometer * 0.6214
+    
+    print(kilometer, "kilometers converted to miles is:", mile, "miles.")
+    
+convert()
